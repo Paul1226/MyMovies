0823d69a9a5b94205c772dde31f3df56a23dccc5
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-11-12 17:01:29 -0500

    Nueva api de la misma página para buscar peliculas

--- src/js/ApiMovie.js
+++ src/js/ApiMovie.js
@@ -1,5 +1,6 @@
 export const urlsTMDB = {
   baseUrl: "https://api.themoviedb.org/3",
   keyApi: "?api_key=246e660d7ee8650b560e49acb51d798b",
+  keyApiSearch: "&api_key=246e660d7ee8650b560e49acb51d798b",
   imgUrl: "https://image.tmdb.org/t/p/w500",
 };
